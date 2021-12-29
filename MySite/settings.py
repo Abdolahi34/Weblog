c0b9511f83f8b7661063fd1a1e4f081c7e01952e
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2021-12-29 12:09:36 +0330

    Debug setting

--- MySite/settings.py
+++ MySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!6vhjf22+u(2jj=!c%a$jy4!p#ia7in1)+e!5+s@tq_v1w8cm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
